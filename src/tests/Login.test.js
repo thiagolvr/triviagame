8fdd70ce15c36b65d0bd2163745d59cb608d2224
Yasmin358 <amorin.yasmin378@gmail.com>
2022-08-15 14:47:35 -0300

    Resolução requisito 4

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,49 +1,69 @@
-// import { screen, fireEvent, waitFor } from '@testing-library/react';
-// import React from 'react';
-// import Login from '../pages/Login';
-// import App from '../App';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-// describe('Testando a página de Login', () => {
-//   test('Testa se os inputs de email e senha são renderizados na tela de Login', () => {
-//     renderWithRouterAndRedux(<Login />);
-//     const imputEmail = screen.getByTestId('input-gravatar-email');
-//     const imputName = screen.getByTestId('input-player-name');
-//     expect(imputEmail).toBeInTheDocument();
-//     expect(imputName).toBeInTheDocument();
-//   })
-//   test('Testa se existem os botões de configurações e o de play na tela de login', () => {
-//     renderWithRouterAndRedux(<Login />);
-//     const playButton = screen.getByRole('button', { name: 'Play' });
-//     const settingsButton = screen.getByRole('button', { name: 'Settings' });
-//     expect(playButton).toBeInTheDocument();
-//     expect(settingsButton).toBeInTheDocument();
-//   })
-//   test('Testa se  o botão está desabilitado caso um dos campos não esteja preenchido', () => {
-//     renderWithRouterAndRedux(<Login />);
-//     const imputEmail = screen.getByTestId('input-gravatar-email');
-//     const imputName = screen.getByTestId('input-player-name');
-//     const playButton = screen.getByRole('button', { name: 'Play' });
-//     imputEmail.value === '' || imputName.value === '' ? expect(playButton).toBeDisabled() : null;
-//   })
-//   test('Testa se ao clicar no botão play é redirecionado para rota /game', async () => {
-//     renderWithRouterAndRedux(<App />);
-//     const playButton = screen.getByRole('button', { name: /play/i });
-//     userEvent.type(screen.getByTestId('input-gravatar-email'), 'algum@email.com');
-//     userEvent.type(screen.getByTestId('input-player-name'), 'alguem');
-//     fireEvent.click(playButton);
-//     await waitFor(() => {
-//      expect(window.location.pathname).toBe('/game/0'),
-//      { timeout: 3000 }
-//     });
-//   })
-//   test('Testa se ao clicar no botão settings é redirecionado para rota /settings', () => {
-//     renderWithRouterAndRedux(<App />);
-//     const settingsButton = screen.getByRole('button', { name: 'Settings' });
-//     fireEvent.click(settingsButton);
-//     const settingsHeading = screen.getByRole('heading', { name: /settings/i})
-//      expect(settingsHeading).toBeInTheDocument();
-//   });
+ import { screen, fireEvent, waitFor } from '@testing-library/react';
+ import React from 'react';
+ import Login from '../pages/Login';
+ import App from '../App';
+ import userEvent from '@testing-library/user-event';
+ import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+ 
+ describe('Testando a página de Login', () => {
+   test('Testa se os inputs de email e senha são renderizados na tela de Login', () => {
+     renderWithRouterAndRedux(<Login />);
+     const imputEmail = screen.getByTestId('input-gravatar-email');
+     const imputName = screen.getByTestId('input-player-name');
+     expect(imputEmail).toBeInTheDocument();
+     expect(imputName).toBeInTheDocument();
+   })
+   test('Testa se existem os botões de configurações e o de play na tela de login', () => {
+     renderWithRouterAndRedux(<Login />);
+     const playButton = screen.getByRole('button', { name: 'Play' });
+     const settingsButton = screen.getByRole('button', { name: 'Settings' });
+     expect(playButton).toBeInTheDocument();
+     expect(settingsButton).toBeInTheDocument();
+   })
+  test('Testa se  o botão está desabilitado caso um dos campos não esteja preenchido', () => {
+     renderWithRouterAndRedux(<Login />);
+     const imputEmail = screen.getByTestId('input-gravatar-email');
+     const imputName = screen.getByTestId('input-player-name');
+     const playButton = screen.getByRole('button', { name: 'Play' });
+     
+     expect(imputEmail).toHaveTextContent('');
+     expect(imputName).toHaveTextContent('');
+     expect(playButton).toBeDisabled();  
+     
+     userEvent.type(imputEmail, 'alguem@email.com');
+     expect(playButton).toBeDisabled();
+  })
+  
+  test('Testa se  o botão está habilitado caso um dos campos esteja preenchido', () => {
+    renderWithRouterAndRedux(<Login />);
+    const imputEmail = screen.getByTestId('input-gravatar-email');
+    const imputName = screen.getByTestId('input-player-name');
+    const playButton = screen.getByRole('button', { name: 'Play' });
+    
+    userEvent.type(imputEmail, 'alguem@email.com');
+    userEvent.type(imputName, 'alguem');
+    expect(playButton).toBeEnabled();  
+ })
+test('Testa se ao clicar no botão play é redirecionado para rota /game', async () => {
+     renderWithRouterAndRedux(<App />);
+     const playButton = screen.getByRole('button', { name: 'Play' });
+     userEvent.type(screen.getByTestId('input-gravatar-email'), 'alguem@email.com');
+     userEvent.type(screen.getByTestId('input-player-name'), 'alguem');
+     fireEvent.click(playButton);
+     
+     await waitFor(() => 
+      expect(screen.getByText('alguem')).toBeInTheDocument(),
+      { timeout: 5000 }
+     );
+      console.log('pathname', location.pathname);
+   })
+   test('Testa se ao clicar no botão settings é redirecionado para rota /settings', () => {
+     renderWithRouterAndRedux(<App />);
+     const settingsButton = screen.getByRole('button', { name: 'Settings' });
+     fireEvent.click(settingsButton);
+     const settingsHeading = screen.getByRole('heading', { name: /settings/i})
+      expect(settingsHeading).toBeInTheDocument();
+   });
 
 //   test('Testa se o localStorage é chamado', () => {
 //     renderWithRouterAndRedux(<App />)
@@ -55,4 +75,4 @@
 
 //     expect(localStorage.getItem).toHaveBeenCalledWith('code');
 //   })
-// })
+})
